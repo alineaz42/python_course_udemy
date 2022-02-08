623066a0938a922a164b6984d0179598b484f5f5
Ali Neaz <neazali77@gmail.com>
2022-02-09 03:07:46 +0600

    till python data types

--- 01_chap01/09_string.py
+++ 01_chap01/09_string.py
@@ -1,1 +1,22 @@
 # sequence of characters"string"
+
+
+your_name = "This is the variable name of your string"
+print(your_name)
+# strings can also be written inside a tripple quote
+my_name = """Hi this is ali neaz"""
+print(my_name)
+his_name = '''Hey are you there'''
+print(his_name)
+# string concatination
+a = "python"
+b = "3"
+print(a+b)
+
+# formatter
+#  %d
+#  %f
+#  %f
+
+a = "python 3"
+print("This is %s" % a)
---
+++ 01_chap01/10_data_types.py
@@ -0,0 +1,20 @@
+# basically there are two type of data
+# 1. Numeric 2. Non-numeric
+# 1. Complex,Integer,Float
+a = 10
+print(type(a))
+# b = 3+a*4
+# print(type(b))
+c = 451.45
+print(type(c))
+
+
+# 2. string boolean, Null etc
+print(type(True))
+x = "hey there"
+print(type(x))
+
+# immutable data types
+# num,str,tuple
+# mutable data types
+# list,distionary,set
